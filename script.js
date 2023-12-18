bf2b94328a95228a3b0c8b0c3a69c22762ca8ff0
gu45 <86247735+gu45@users.noreply.github.com>
2023-12-18 12:06:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function stringChop(str, size) {
   // your code here
+	  let ans = []
+  let i=0;
+  if(str===null){
+    return []
+  }
+  while(i<str.length){
+    ans.push(str.slice(i,size))
+    i = i+5;
+    size = size+5;
+  }
+  return ans
 }
 
 // Do not change the code below
